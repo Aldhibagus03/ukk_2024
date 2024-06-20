29b954af3e18b431d5b314fea5f8bba3e205c42e
Aldhibagus03 <aldhibagus03@gmail.com>
2024-06-20 18:03:56 +0700

    [invent-01]Lihat Semua Kategori

---
+++ app/Http/Controllers/Api/KategoriController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Kategori;
+use Illuminate\Http\Request;
+
+class KategoriController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $kategori = Kategori::all();
+        $data = array("data"=>$kategori);
+
+        return response()->json($data);
+    }
+}
